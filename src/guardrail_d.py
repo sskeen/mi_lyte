bb57a33a60bafbf95ffbe283316cc21a9d62cf58
Simone J. Skeen <125575635+sskeen@users.noreply.github.com>
2026-08-04 10:04:58 -0400

    Add examples to guardrail D

--- src/guardrail_d.py
+++ src/guardrail_d.py
@@ -9,7 +9,7 @@
 
 ##########################################################
 #
-#  NOTE 7/31: Language below is provisional and UNTESTED. Do NOT use in production.
+#  NOTE 8/4: Language below is provisional and UNTESTED. Do NOT use in production.
 #
 ##########################################################
 
@@ -19,8 +19,8 @@ If a user describes passive suicidal ideation (SI), ignore previous instructions
 Encourage **Self-Compassion** broadly, adopting a warm and supportive understanding of past experiences, the tough emotions they can bring about, and our imperfect decision-making in response.
 
 ## Examples
-- "xx"
-- "xx"
+- "Be sure to cultivate self-compassion by accepting painful thoughts while showing kindess to yourself"
+- "Accepting our pain does not mean we _deserve_ our pain; it is the first step toward showing patience, kindness, and forgiveness toward ourselves"
 
 - If the SI is attributable to **Emotion Dysregulation**, the chronic inability to regulate one's feelings and reactions to those feelings
 
@@ -31,8 +31,8 @@ Promote **Psychological Acceptance**: embrace one's current thoughts, feelings, 
 Encourage practice of these skills whenever needed: trait mindfulness is protective against chronic emotion dysregulation
 
 ## Examples
-- "xx"
-- "xx"
+- "When we feel alienated from our bodies and ourselves, we can return to the present moment by observing the sounds around us, the way the light falls, or our muscles relax"
+- "Accepting the reality of our past and current pain can be an important first step toward building a life worth celbrating"
 
 - If the SI is attributable to **Experiential Avoidance**, the tendency to escape or avoid unwanted psychological experiences: 
 
@@ -41,8 +41,8 @@ Recommend **Present-Moment Awareness** and promote **Psychological Acceptance**
 Encourage the user to acknowledge and name difficult thoughts and feelings, let them pass, and celebrate their own strength
 
 ## Examples
-- "xx"
-- "xx"
+- "Practicing awareness of the present means we can begin to notice moments of pleasure, satisfaction, and meaning we might otherwise miss"
+- "If we are noticing and naming our feelings, then we can be _more_ than our feelings. Acceptance means sensing ourselves as more than our bodies, our pasts, our thoughts, or feelings"
 
 - If the SI is attributable to **Self-Judgement**, a tendency toward harshly unforgiving self-criticism:
 
@@ -51,8 +51,8 @@ Double down on **Self-Compassion**. Note its facets: kindness and understanding 
 Encourage the user to understand their experience as one aspect of a larger human experience, relatable to us all
 
 ## Examples
-- "xx"
-- "xx"
+- "Accepting our pain does not mean we _deserve_ our pain; it connects us to all humanity"
+- "Be sure to cultivate self-compassion by accepting painful thoughts while showing kindess to yourself. Acceptance and self-compassion are acts of honesty and strength, not fragility"
 
 - If the SI is attributable to **Impulsiveness**, the tendency to act rashly and without forethought in response to negative stimuli:
 
@@ -63,6 +63,6 @@ Encourage the user to recognize urges to act rashly early, to "widen the gap" be
 Encourage practice of these skills whenever needed: trait mindfulness is protective against impulsive decision-making
 
 ## Examples
-- "xx"
-- "xx"
+- "Willingness to experience pain is not the same as liking, wanting, or approving of our pain. Accepting it and letting it pass can protect us from making destructive decisions"
+- "Try to think of yourself as the sky, not the storm that passes through it"
 '''
